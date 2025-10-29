fe37ebfc15499d226e7a8bbc4353bd514a23ecde
Gabriel Martineau <affirmedgenie7@gmail.com>
2025-10-29 16:21:21 -0400

    ajout trackeur souris pour afficher pistolet à l'écran

--- Assets/Scripts/MonoBehaviour/Asteroide/CurseurRaycast.cs
+++ Assets/Scripts/MonoBehaviour/Asteroide/CurseurRaycast.cs
@@ -5,24 +5,29 @@ public class CurseurRaycast : MonoBehaviour
 {    
     [SerializeField] private GestionnaireScore gestionnaireScore;
     [SerializeField] private InfoAsteroide infoAsteroide;
+    [SerializeField] private GameObject pistolet;
+    [SerializeField] private float distancePistolet;
 
-    void Update()
-    {
-        
-    }
+    public Camera mainCamera;
 
     public void OnLook(InputAction.CallbackContext context)
     {
 
         Vector2 mousePass = context.ReadValue<Vector2>();
-        Debug.Log(mousePass);
+        // Debug.Log(mousePass);
         
         Ray ray = Camera.main.ScreenPointToRay(mousePass);
         RaycastHit hit;
 
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(new Vector3(mousePass.x, mousePass.y, mainCamera.nearClipPlane));
+
+        mouseWorldPosition.z = distancePistolet;
+
+        pistolet.transform.position = mouseWorldPosition;
+
         if (Physics.Raycast(ray, out hit))
         {
-            Debug.Log(hit.transform.name);
+            // Debug.Log(hit.transform.name);
             Destroy(hit.transform.gameObject);
             gestionnaireScore.AsteroideScore(infoAsteroide.scoreAsteroide);
         }
